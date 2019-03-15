92601cdcd9a0c323949f1acf6a324a719e348ce5
a-visai <vce.visai@gmail.com>
2019-03-15 16:12:10 -0700

    Ruby challenges done

--- rails-intro-assessment.rb
+++ rails-intro-assessment.rb
@@ -8,7 +8,12 @@
 #hello_world("es") should return "Hola Mundo"
 #hello_world("de") should return "Hallo Welt"
 
+def hello_world(lang)
+  lang == "es" ? result = "Hola Mundo" : lang == "de" ? result = "Hallo Welt" : result = "Hello World"
+  p result
+end
 
+hello_world("es")
 
 # The Grade Assigner
 # Write a Method named assign_grade that:
@@ -17,6 +22,11 @@
 # returns a grade for the score, either "A", "B", "C", "D", or "F".
 # assign_grade(90) should return A
 # assign_grade(75) should return C
+
+def assign_grade(score)
+  score >= 90 ? grade = "A" : score >= 80 ? grade = "B" : score >= 70 ? grade = "C" : score >= 60 ? grade = "D" : grade = "F"
+  p grade
+end
 
 
 # The Pluralizer
@@ -28,4 +38,16 @@
 # pluralizer(1, dog) should return "1 dog" because the number one does not require a plural noun
 # Arguments will always be a number and a singular noun.
 
+def pluralizer(num,noun)
+  p num > 1 ? "#{num} #{noun}s" : nil
+end
+
+
 # Bonus: Make it handle a few collective nouns like "sheep", "geese", "children", "people" and "species".
+
+def pluralizer(num,noun)
+  p noun == "sheep" ? "#{num} #{noun}" : noun == "geese" ? "#{num} #{noun}" :
+    num == 1 && noun == "child" ? "#{num} #{noun}" : noun == "child" ? "#{num} #{noun}ren" :
+    noun == "children" ? "#{num} child" : num == 1 && noun == "people" ? "#{num} person" :
+    num > 1 ? "#{num} #{noun}s" : nil
+end
